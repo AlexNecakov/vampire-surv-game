2b0bb24bf4005b6079e6fcb5794c7a184bd970e5
Alex Necakov <alexnecakov@gmail.com>
2024-07-10 22:49:27 -0400

    dude moving

--- entry_udg.c
+++ entry_udg.c
@@ -1,23 +1,53 @@
 
 int entry(int argc, char **argv) {
 	
-	window.title = STR("Minimal Game Example");
+	window.title = STR("Untitled Dungeon Game");
 	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
 	window.scaled_height = 720; 
 	window.x = 200;
 	window.y = 90;
 	window.clear_color = hex_to_rgba(0x6495EDff);
 
-	while (!window.should_close) {
+    Gfx_Image* player = load_image_from_disk(fixed_string("asesprite\\dude.png"), get_heap_allocator());
+    assert(player, "player image didnt load");
+
+	Vector2 player_pos = v2(0,0);
+	const float32 input_speed = 1.0;
+
+    float64 last_time = os_get_current_time_in_seconds();
+
+    while (!window.should_close) {
 		reset_temporary_storage();
+	
+        float64 now = os_get_current_time_in_seconds();
+		float64 delta = now - last_time;
+		last_time = now;	
+
+        os_update(); 
+	
+        if (is_key_just_pressed(KEY_ESCAPE)){
+            window.should_close = true;
+        }
+
+		Vector2 input_axis = v2(0, 0);
+		if (is_key_down('A')) {
+			input_axis.x -= 1.0;
+		}
+		if (is_key_down('D')) {
+			input_axis.x += 1.0;
+		}
+		if (is_key_down('S')) {
+			input_axis.y -= 1.0;
+		}
+		if (is_key_down('W')) {
+			input_axis.y += 1.0;
+		}
+
+		player_pos = v2_add(player_pos, v2_mulf(input_axis, input_speed * delta));
 		
-		os_update(); 
-		
-		float64 now = os_get_current_time_in_seconds();
-		Matrix4 rect_xform = m4_scalar(1.0);
-		rect_xform         = m4_rotate_z(rect_xform, (f32)now);
-		rect_xform         = m4_translate(rect_xform, v3(-.25f, -.25f, 0));
-		draw_rect_xform(rect_xform, v2(.5f, .5f), COLOR_GREEN);
+		Matrix4 xform = m4_scalar(1.0);
+		xform = m4_translate(xform, v3(v2_expand(player_pos), 0));
+		draw_image_xform(player, xform, v2(.5f, .5f), COLOR_WHITE);
 		
 		gfx_update();
 	}
