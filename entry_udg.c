f7eed3c8101859ef601bed82621c844d09117fad
Alex Necakov <alexnecakov@gmail.com>
2024-07-19 12:16:41 -0400

    entity struct initial

--- entry_udg.c
+++ entry_udg.c
@@ -1,3 +1,56 @@
+typedef enum EntityArchetype{
+    arch_nil = 0,
+    arch_player = 1,
+    arch_monster = 2,
+} Archetype;
+
+typedef struct Entity{
+    bool is_valid;
+    Archetype arch;
+    Vector2 pos;
+    
+    bool render_sprite;
+    Vector2 size;
+    Gfx_Image* sprite;
+} Entity;
+
+#define MAX_ENTITY_COUNT 1024
+
+typedef struct World{
+    Entity entities[MAX_ENTITY_COUNT];
+} World;
+World* world = 0;
+
+Entity* entity_create() {
+    Entity* entity_found = 0;
+    for (int i = 0; i < MAX_ENTITY_COUNT; i++){
+        Entity* existing_entity = &world->entities[i];
+        if (!existing_entity->is_valid){
+            entity_found = existing_entity;
+            break;
+        }
+    }
+    assert(entity_found, "No more free entities!");
+    entity_found->is_valid = true;
+    return entity_found;
+}
+
+void entity_destroy(Entity* entity){
+    memset(entity, 0, sizeof(Entity));
+}
+
+void setup_player(Entity* en) {
+    en->arch = arch_player;
+    en->sprite = load_image_from_disk(fixed_string("asesprite\\dude.png"), get_heap_allocator());
+    en->size = (Vector2){en->sprite->width, en->sprite->height};
+}
+
+void setup_spider(Entity* en) {
+    en->arch = arch_monster;
+    en->sprite = load_image_from_disk(fixed_string("asesprite\\spider.png"), get_heap_allocator());
+    en->size = (Vector2){en->sprite->width, en->sprite->height};
+}
+
 int entry(int argc, char **argv) {
 	
 	window.title = STR("The Dark");
@@ -5,8 +58,10 @@ int entry(int argc, char **argv) {
 	window.scaled_height = 720; 
     window.x = 200;
     window.y = 200;
-	window.clear_color = hex_to_rgba(0x6495EDff);
+	window.clear_color = hex_to_rgba(0x1e1e1eff);
     float32 aspectRatio = (float32)window.width/(float32)window.height; 
+    
+    world = alloc(get_heap_allocator(), sizeof(World));
 
     float32 spriteSheetWidth = 240.0;
     float32 zoom = window.width/spriteSheetWidth;
@@ -15,15 +70,17 @@ int entry(int argc, char **argv) {
 	assert(font, "Failed loading arial.ttf, %d", GetLastError());
 	
 	render_atlas_if_not_yet_rendered(font, 32, 'A');
-    
-    Gfx_Image* player = load_image_from_disk(fixed_string("asesprite\\dude.png"), get_heap_allocator());
-    assert(player, "player image didnt load");
-
-    Gfx_Image* playerAtk = load_image_from_disk(fixed_string("asesprite\\dude_attack.png"), get_heap_allocator());
-    assert(playerAtk, "player image didnt load");
 	
-    Vector2 player_pos = v2(0,0);
 	float32 input_speed = 50.0;
+
+    Entity* player_en = entity_create();
+    setup_player(player_en);
+
+    for (int i = 0; i < 10; i++){
+        Entity* en = entity_create();
+        setup_spider(en);
+        en->pos = v2(i * 10.0, 0.0);
+    }
 
     float64 seconds_counter = 0.0;
     s32 frame_count = 0;
@@ -63,15 +120,26 @@ int entry(int argc, char **argv) {
         }
         
         input_axis = v2_normalize(input_axis);
-        
-        player_pos = v2_add(player_pos, v2_mulf(input_axis, input_speed * delta ));
 
-        Vector2 size = v2(player->width, player->height);
-        Matrix4 xform = m4_scalar(1.0);
-        xform = m4_translate(xform, v3(v2_expand(player_pos), 0));
-        xform = m4_translate(xform, v3(size.x * -0.5, 0.0, 0));
-        draw_image_xform(player, xform, v2(player->width, player->height), COLOR_WHITE);
-        
+        for (int i = 0; i < MAX_ENTITY_COUNT; i++){
+            Entity* en = &world->entities[i];
+            if (en->is_valid){
+                switch (en->arch){
+                    case arch_player:
+                        en->pos = v2_add(en->pos, v2_mulf(input_axis, input_speed * delta ));
+                        break;
+                    case arch_monster:
+                        break;
+                    default:
+                        break;
+                }
+                Matrix4 xform = m4_scalar(1.0);
+                xform = m4_translate(xform, v3(v2_expand(en->pos), 0));
+                xform = m4_translate(xform, v3(en->size.x * -0.5, 0.0, 0));
+                draw_image_xform(en->sprite, xform, en->size, COLOR_WHITE);
+            }
+        }
+
 		gfx_update();
         
         seconds_counter += delta;
